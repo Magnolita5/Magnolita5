6b57b8113548b066e275e849d4a616cc4c5b27a1
Maria M. Mezones <mariamezomes10@gmail.com>
2024-09-10 15:02:24 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print('Mariela Zapata')
+print('4520 Dracea Ave')
+print('Pomona, CA 78903')
+print('225-777-2121')
+
+print('Sales Prediction')
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
