27b645f26b5b70ea08ef0bed5bedec023975c519
Maria M. Mezones <mariamezomes10@gmail.com>
2024-09-10 15:50:04 -0700

    Add files via upload

---
+++ landcalc.py
@@ -0,0 +1,9 @@
+print(1+2)
+print(3-1)
+print(2*2)
+print(4/2)
+Units_square_feet = 43560
+unit_acre = 1 
+square_feet = int(input("enter square feet"))
+acre = square_feet / Units_square_feet
+print(acre)
